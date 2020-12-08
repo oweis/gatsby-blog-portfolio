20808baba9b56f43f5af96a1f81910e2e355f9ec
Yahya Oweis <oweisyahya@gmail.com>
2020-12-08 14:32:35 +0100

    change website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Oweis Yahya - Software Engineer',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+       'This repo follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution. build with Gatsby and Netlify CMS',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
