b3e551c3ec2ec90e082eab9a837db9fff4c09778
Yahya Oweis <oweisyahya@gmail.com>
2020-12-08 14:30:15 +0100

    remove unnecessary related contact form field

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,7 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>Thank you for getting in touch, i will contact you soon.</p>
         </div>
       </div>
     </section>
