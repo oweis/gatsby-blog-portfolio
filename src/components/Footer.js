79bcef94517d478937d608e36c13de1110200552
Yahya Oweis <oweisyahya@gmail.com>
2020-12-08 14:34:02 +0100

    Remove admin link from website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,16 +39,6 @@ const Footer = class extends React.Component {
                         Projects
                       </Link>
                     </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
                   </ul>
                 </section>
               </div>
